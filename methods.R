cf031c1f8dfaf585837d116a33cdd1599ee5134c
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-04-18 02:04:12 -0400

    Add files via upload

--- methods.R
+++ methods.R
@@ -1,17 +1,50 @@
-parse_content=function(cluster_file,expr_file){
-	print("------------------------------------------------")
-	print(paste0("parse_content::",cluster_file))
-	data=read.csv(cluster_file,sep=" ",header=T)
-	u_cols=unique(data[,1])
+print_modules=function(d1){
+	u_cols=unique(d1[,1])
 	for(x in 1:length(u_cols)){
-		N=subset(data,data[,1]==u_cols[x])
+		N=subset(d1,d1[,1]==u_cols[x])
 		N=dim(N)[1]
 		print(c(u_cols[x],N))
 	}
+}
+
+parse_content=function(cluster_file,expr_file){
+	print("------------------------------------------------")
+	print(paste0("parse_content::",cluster_file))
+	if(!exists("d1")){
+		d1<<-read.csv(cluster_file,sep=" ",header=T)
+		print_modules(d1)
+	}else{
+		print(paste0("d1 already exists"))
+		print(dim(d1))
+	}
+
+	my_dark_green=subset(d1,d1[,1]=="darkgreen")
 
 	print("------------------------------------------------")
 	print(paste0("parse_content::",expr_file))
-	data=read.csv(expr_file,sep="\t",header=T)
-	print(head(data))
+	if(!exists("d2")){
+		d2<<-read.csv(expr_file,sep="\t",header=T)
+	}else{
+		print(paste0("d2 already exists"))
+		print(dim(d2))
+	}
 	print("------------------------------------------------")
-}
+
+	d2_expr=subset(d2,d2[,1]%in%my_dark_green[,2])
+	print(dim(d2_expr))
+	print(colnames(d2_expr))
+
+	ix1=grep("longRAF",colnames(d2_expr))
+	ix2=grep("longRAM",colnames(d2_expr))
+	
+	A=c()
+	B=c()
+	for(x in 1:dim(d2_expr)){
+		val1=as.numeric(d2_expr[x,ix1])
+		val2=as.numeric(d2_expr[x,ix2])
+		A=c(A,mean(val1))
+		B=c(B,mean(val2))
+	}
+	print(t.test(A,B)$p.value)
+	print(wilcox.test(A,B)$p.value)
+}	
