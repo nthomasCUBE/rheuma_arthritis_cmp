4181334f72647c8fc9c14d7f92f506f35510059d
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-05-02 14:25:28 -0400

    Add files via upload

--- methods.R
+++ methods.R
@@ -1,5 +1,32 @@
 options(stringsAsFactors=FALSE)
 
+make_correlation=function(file4){
+	print(c("make_correlation"))
+
+	data=read.csv(file4,sep=" ",header=T)
+	corrplot(as.matrix(data),is.corr=F)
+}
+
+
+make_anova=function (my_data) 
+{
+	u_c=unique(my_data[,4])
+	print(head(my_data))
+	x1_all=c()
+	x2_all=c()
+	for(x in 1:length(u_c)){
+		print(u_c[x])
+		e1=subset(my_data,my_data[,4]==u_c[x])
+		x1=e1[,3]-e1[,2]
+		x2=e1[,4]
+		x1_all=c(x1_all,x1)
+		x2_all=c(x2_all,x2)
+	}	
+	df=data.frame(x1_all,x2_all)
+	print(df)
+	print(summary(aov(df[,1]~df[,2])))
+}
+
 #
 #
 #
@@ -83,10 +110,17 @@ parse_content=function(cluster_file,expr_file){
 
 	df=data.frame()
 	pdf("hist.pdf")
+	
+	A_all=c()
+	B_all=c()
+	C_all=c()
+	D_all=c()
+	
 	par(mfrow=c(3,3))
 	for(mo in 1:length(all_mods)){
 		cur_mod=subset(d1,d1[,1]==all_mods[mo])
 		d2_expr=subset(d2,d2[,1]%in%cur_mod[,2])
+		print(c(all_mods[mo],dim(d2_expr)[1]))
 		for(mc in 1:length(my_comp1)){
 			ix1=grep(my_comp1[mc],colnames(d2_expr))
 			ix2=grep(my_comp2[mc],colnames(d2_expr))
@@ -97,7 +131,12 @@ parse_content=function(cluster_file,expr_file){
 				val2=as.numeric(d2_expr[x,ix2])
 				A=c(A,mean(val1))
 				B=c(B,mean(val2))
+				A_all=c(A_all,mean(val1))
+				B_all=c(B_all,mean(val2))
+				C_all=c(C_all,all_mods[mo])
+				D_all=c(D_all,my_comp1[mc])
 			}
+
 			my_wx=(wilcox.test(A,B,paired=TRUE)$p.value)
 			my_t=(t.test(A,B,paired=TRUE)$p.value)
 			df=rbind(df,c(all_mods[mo],my_comp1[mc],my_t,my_wx))
@@ -110,6 +149,9 @@ parse_content=function(cluster_file,expr_file){
 	df=cbind(df,p.adjust(df[,3]))
 	df=cbind(df,p.adjust(df[,4]))
 	df=subset(df,df[,5]<0.05 | df[,6]<0.05)
+
+	df2=data.frame(A_all,B_all,C_all,D_all)
+	write.xlsx(df2,"prep_lm_24april19.xlsx")
 
 	library(xlsx)
 	colnames(df)=c("module","comparison","t.test","wilcox.test_paired","t.test_adj","wilcox.test_paired_adj")
