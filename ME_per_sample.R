db5b408c7578b119a2536c80deb0552655c0b0ec
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-05-03 20:09:59 +0200

    Update ME_per_sample.R

--- ME_per_sample.R
+++ ME_per_sample.R
@@ -1,6 +1,7 @@
 function(){
 
 	library(WGCNA)
+	library(xlsx)
 
 	options(stringsAsFactors=FALSE)
 	
@@ -12,19 +13,30 @@ function(){
 
 	if(!exists("modul")){
 		print("loading modul...")
-		modul<<-read.csv("C:/Users/thoma/Google Drive/mirror_TN_VM/mirror/2_WGCNA/archive01/gene_WGCNA_clustering_31aug18.txt",sep=" ",header=T)
+		modul<<-read.csv("C:/Users/thoma/Google Drive/mirror_TN_VM/mirror/2_WGCNA/gene_WGCNA_clustering_pooled_3sep18.txt",sep=" ",header=T)
 		print(dim(modul))
 	}
+
+
+	DF=data.frame()
 
 	u_cls=unique(modul[,1])
 	for(x in 1:length(u_cls)){
 		print(u_cls[x])
 		my_g=subset(modul,modul[,1]==u_cls[x])
-		my_e=subset(expr,expr[,2]%in%my_g[,2])
+		my_e=subset(expr,expr[,1]%in%my_g[,2])
 		my_e=my_e[,3:(dim(my_e)[2]-3)]
-		MEList = moduleEigengenes(my_e,rep("yellow",dim(my_e)[2]))
+		print(dim(my_e))
+		MEList = moduleEigengenes(t(my_e),rep(u_cls[x],dim(my_e)[1]))
 		MEs = MEList$eigengenes
-		#plotEigengeneNetworks(MEs, "", marDendro = c(0,4,1,2), marHeatmap = c(3,4,1,2))
-
+		print(MEs)
+		if(x==1){
+			DF=data.frame(MEs)
+		}else{
+			DF=cbind(DF,MEs)
+		}
 	}
+
+	write.xlsx(DF,"DF_3may19.xlsx")
+
 }
