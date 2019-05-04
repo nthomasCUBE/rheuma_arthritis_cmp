c3ccfae812812e5442ec03716a4876109668ddf0
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-05-04 19:20:57 +0200

    Update ME_per_sample.R

--- ME_per_sample.R
+++ ME_per_sample.R
@@ -1,4 +1,4 @@
-ME_per_sample=function(){
+function(){
 
 	library(WGCNA)
 	library(xlsx)
@@ -7,28 +7,36 @@ ME_per_sample=function(){
 	
 	if(!exists("expr")){
 		print("loading expr...")
-		expr<<-read.csv("../mirror/data/combinedFPKMs_highestTranscripts.txt",sep="\t",header=T)
+		expr<<-read.csv("C:/Users/thoma/Google Drive/mirror_TN_VM/mirror/data/combinedFPKMs_highestTranscripts.txt",sep="\t",header=T)
 		print(dim(expr))
 	}
 
 	if(!exists("modul")){
 		print("loading modul...")
-		modul<<-read.csv("../mirror/2_WGCNA/gene_WGCNA_clustering_pooled_3sep18.txt",sep=" ",header=T)
+		modul<<-read.csv("C:/Users/thoma/Google Drive/mirror_TN_VM/mirror/2_WGCNA/gene_WGCNA_clustering_pooled_3sep18.txt",sep=" ",header=T)
 		print(dim(modul))
 	}
 
+
 	DF=data.frame()
+
 	u_cls=unique(modul[,1])
-	my_E=data.frame()
-	my_C=c()
 	for(x in 1:length(u_cls)){
 		print(u_cls[x])
 		my_g=subset(modul,modul[,1]==u_cls[x])
 		my_e=subset(expr,expr[,1]%in%my_g[,2])
 		my_e=my_e[,3:(dim(my_e)[2]-3)]
-		my_E=rbind(my_E,my_e)
-		my_C=c(my_C,rep(u_cls[x],dim(my_e)[1]))
+		print(dim(my_e))
+		MEList = moduleEigengenes(t(my_e),rep(u_cls[x],dim(my_e)[1]))
+		MEs = MEList$eigengenes
+		print(MEs)
+		if(x==1){
+			DF=data.frame(MEs)
+		}else{
+			DF=cbind(DF,MEs)
+		}
 	}
-	MEList = moduleEigengenes(t(my_E),my_C)
-	write.xlsx(MEList$eigengene,"DF2_V2_4may19.xlsx")
+
+	write.xlsx(DF,"DF_3may19.xlsx")
+
 }
